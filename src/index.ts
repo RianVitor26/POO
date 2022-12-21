0d4a7db76fabf7c71c491b4305d7a4f9c9e72fd3
RianVitor26 <rianvitorlhe@gamil.com>
2022-12-21 02:06:56 -0300

    aplicando herança na classe athlete da classe person

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,21 @@ class Person {
 
 const person1 = new Person("Rian", 20, 1.80, 80)
 
-console.log(`Nome: ${person1.name}, age: ${person1.age}, height: ${person1.height}, weight: ${person1.weight}`)
+console.log(`PESSOA --- Nome: ${person1.name}, age: ${person1.age} years old, height: ${person1.height}tall, weight: ${person1.weight}kg`)
+
+
+class Athlete extends Person{
+    sport: string = ""
+    modality: string = ""
+
+    constructor(name: string, age: number, height: number, weight: number, sport:string, modality:string) {
+        super(name, age, height, weight)
+        this.sport = sport
+        this.modality = modality
+    }
+}
+
+const athlete1 = new Athlete("Rian", 20, 1.80, 80, "football", "field football")
+console.log(
+  `ATHLETE --- Nome: ${athlete1.name}, age: ${athlete1.age} years old, height: ${athlete1.height}tall, weight: ${athlete1.weight}kg, Sport: ${athlete1.sport}, Modality: ${athlete1.modality}'}`
+);
